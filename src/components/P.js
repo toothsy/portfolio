3acef611ff874ae4ea99cd34640948079396dd86
toothsy <c.j.atharva@gmail.com>
2021-04-09 18:40:22 +0530

    minor mistake

--- src/components/P.js
+++ src/components/P.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import mAC from '../svg/manAndCar.svg'
 import backG from '../svg/backG.svg'
-import Card from './Card'
+import Card from './C'
 
 const projects=()=>{
 let cardsInfo = [{
