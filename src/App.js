c07fcfb46c83b1e66f7323f661a5b54292fd7ced
toothsy <c.j.atharva@gmail.com>
2021-04-09 18:02:39 +0530

    hopefully this fiex it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // eslint-disable-next-line
 import React,{createContext} from 'react'
 import Home from './components/Homepage'
-import CM from './components/ContactMe'
 import AM from './components/aboutMe'
 import Pro from './components/projects'
 import Art from './components/Articles'
 import Menu from './components/Menu'
 import Footer from './components/footer'
+import CM from './components/ContactMe'
 
 export default function App() {
   return (
