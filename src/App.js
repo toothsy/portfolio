ba622a6f6c73492e55b35f881ff57a1fce97e9fc
toothsy <c.j.atharva@gmail.com>
2021-04-15 13:01:41 +0530

    final commit for pwa

--- src/App.js
+++ src/App.js
@@ -6,27 +6,8 @@ import Pro from './components/P'
 import Art from './components/A'
 import Menu from './components/M'
 import Footer from './components/F'
-import CM from './components/CM'
-import swal from  'sweetalert'
+import CM from './components//CM'
 export default function App() {
-window.addEventListener('beforeinstallprompt',async  (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  window.deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  try{let choice = await swal({
-      title: "Are you sure you want to install?",
-      text: "I will be added to your homescreen",
-      icon: "info",
-      buttons: true
-    })
-    choice?console.log("installed succesfully"):console.log("cant install")
-      }
-    catch(e){console.log("error is here \n\n",e)}
-    window.deferredPrompt =null;
-});
-
   return (
     < >
             <Menu/>
