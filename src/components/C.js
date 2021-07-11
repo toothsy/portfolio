bfc503cc7da91ccd55f78285439516ceef73488e
toothsy <c.j.atharva@gmail.com>
2021-07-11 13:05:35 +0530

    added dbms project

--- src/components/C.js
+++ src/components/C.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import i from '../svg/i.svg'
 import close from '../svg/close.svg'
-export default function Card({sf,lf,r,link,id}) {
+export default function Card({sf,lf,r,link,id,gold}) {
      // let width = window.innerWidth
      const makeInfoVisible = (e) =>{
           let id = e.target.dataset.id
           let aage = document.querySelectorAll('.aage')[id];
           let peeche = document.querySelectorAll('.peeche')[id];
           let close = document.querySelectorAll('.close')[id];
+          // console.log(document.querySelectorAll('.aage'));
+          // console.log(document.querySelectorAll('.peeche'));
+          // console.log(document.querySelectorAll('.close'));
           aage.classList.toggle("upar")
           peeche.style.opacity=1
           peeche.style.transform="scale(1)"
@@ -29,11 +32,11 @@ export default function Card({sf,lf,r,link,id}) {
      
      return (
           <div className="card">
-               <div className="aage">
+               <div className={`aage ${gold?"gold":""}`}>
                     <img src={i } alt="i" onClick={makeInfoVisible} className="i" data-id={id} />
                     <img src={close} alt="close" onClick={makeInfoInvisible} className="close" data-id={id} />
-                    <h2 className="sf"> {sf}</h2>
-                    <h3 className="lf">{lf}</h3>
+                    <h2 className={`sf ${gold?"gold-sf":""}`}> {sf}</h2>
+                    <h3 className={`lf ${gold?"gold-lf":""}`}>{lf}</h3>
                </div>
                <div className="peeche" >
                     <h3 className="desc">{r}</h3>
--- src/components/P.js
+++ src/components/P.js
@@ -3,25 +3,41 @@ import mAC from '../svg/manAndCar.svg'
 import backG from '../svg/backG.svg'
 import Card from './C'
 const projects=()=>{
-let cardsInfo = [{
+    let firstRow=[
+        {
+            "sf":"S A S",
+            "lf":"Smart Attendance System",
+            "right":"this is a Full Stack Project which also includes face detection service" ,
+            "link":"https://github.com/slirq/Smart-attendance-database-system",
+            "gold":true,
+            "id":0
+            }
+    ]
+let secondRow = [{
         "sf":"CG",
         "lf":"Color Game",
         "right":"This is a simple game about guessing a color from the given rgb value." ,
         "link":"https://toothsy.github.io/colorGame/",
-        "id":0
+        "gold":false,
+        "id":1
     },
     {
         "sf":"3-P",
         "lf":"3-Page-demo",
         "right":"A basic implementation of server authentication with a log-in and registering" ,
         "link":"https://simple-3-page-demo.netlify.app/",
-        "id":1},
+        "gold":false,
+        "id":2},
     {
         "sf":"R A",
         "lf":"Rest Api demo",
         "right":"Here the site renders data fetched from jsonplace-holder site and is using material-table" ,
         "link":"https://freedom-boi.netlify.app/",
-        "id":2}
+        "gold":false,
+        "id":3
+        
+        }
+
 ]
     return (
         <div id="project" className="container dark">
@@ -34,7 +50,24 @@ let cardsInfo = [{
                 <div className="Right-content">
                     <h1 className="titleDark">Projects</h1>
                         <div className="card-box">
-                        {cardsInfo.map((cardInfo,index)=><Card sf={cardInfo.sf} lf={cardInfo.lf} r={cardInfo.right} id={cardInfo.id} link={cardInfo.link} key={index} />)}
+                        <div className="card-row">
+                            {firstRow.map((cardInfo,index)=><Card sf={cardInfo.sf} 
+                                                                                                lf={cardInfo.lf} 
+                                                                                                r={cardInfo.right} 
+                                                                                                id={cardInfo.id} 
+                                                                                                link={cardInfo.link} 
+                                                                                                key={index}
+                                                                                                gold={cardInfo.gold} />)}
+                        </div>
+                        <div className="card-row">
+                            {secondRow.map((cardInfo,index)=><Card sf={cardInfo.sf} 
+                                                                                                lf={cardInfo.lf} 
+                                                                                                r={cardInfo.right} 
+                                                                                                id={cardInfo.id} 
+                                                                                                link={cardInfo.link} 
+                                                                                                key={index}
+                                                                                                gold={cardInfo.gold} />)}
+                        </div>
                         </div>
                 </div>
             </div>
